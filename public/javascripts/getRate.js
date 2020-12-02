9fe6a3c51764c97bdf3ee67b9842a60b872611dc
dneves02 <dneves02@gmail.com>
2020-12-01 16:54:19 -0800

    append

--- public/javascripts/getRate.js
+++ public/javascripts/getRate.js
@@ -3,6 +3,8 @@ function getRate(){
 
     var type = $('#type').val();
     var weight = $('#weight').val();
+    console.log('Calculating rate for parcel type: '+ type);
+    console.log('Weight: '+ weight);
     
     calculateCost(type, weight);
   
@@ -22,7 +24,7 @@ function calculateCost(type, weight) {
           case '<= 3.5':
             cost = 1.0;
         }
-          
+        console.log('Cost: '+ cost);  
     } else if (type == "metered") {
         switch (weight){
           case '<= 1': 
@@ -34,7 +36,7 @@ function calculateCost(type, weight) {
           case '<= 3.5':
             cost = 0.95;
         } 
-
+        console.log('Cost: '+ cost);  
     } else if (type == "flats") {
         switch (weight){
           case '<= 1': 
@@ -64,6 +66,7 @@ function calculateCost(type, weight) {
           case '<= 13':
             cost = 3.4;
         }
+        console.log('Cost: '+ cost);
 
     } else if (type == "first-class") {
         switch (weight){
@@ -76,57 +79,10 @@ function calculateCost(type, weight) {
           case '<= 13':
             cost = 5.9;
         }
-
+        console.log('Cost: '+ cost);
     }
-    
-    $('#cost').append('$' + cost);
+    console.log('Your total cost is: '+ cost);
+    $('#cost').append('<h3>$' + cost + '</h3>');
       
 }
 
-/* 
-cost.empty();
-    cost.append();
-    
-flats
-1	$1.00
-2	1.20
-3	1.40
-4	1.60
-5	1.80
-6	2.00
-7	2.20
-8	2.40
-9	2.60
-10	2.80
-11	3.00
-12	3.20
-13	3.40
-
-first-class
-1	$3.80	
-2	3.80	
-3	3.80	
-4	3.80	
-5	4.60	
-6	4.60	
-7	4.60	
-8	4.60	
-9	5.30	
-10	5.30	
-11	5.30
-12	5.30
-13	5.90 
-
-stamped
-1	$0.55
-2	0.70
-3	0.85
-3.5	1.00
-
-metered
-1	$0.50
-2	0.65
-3	0.80
-3.5	0.95
-
-*/
