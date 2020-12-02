08529d4b953dfab26797d0e24a081212cad6fa45
dneves02 <dneves02@gmail.com>
2020-12-01 16:00:58 -0800

    switch case

--- public/javascripts/getRate.js
+++ public/javascripts/getRate.js
@@ -12,58 +12,81 @@ function calculateCost(type, weight) {
     var cost = 0.0;
 
     if (type == "stamped") {
-      switch (weight){
-        case '< 1': 
-          cost = 0.55;
-        case '< 2':
-          cost = 0.7;
-        case '< 3':
-          cost = 0.85;
-        case '< 3.5':
-          cost = 1.0;
-      }  
+        switch (weight){
+          case '<= 1': 
+            cost = 0.55;
+          case '<= 2':
+            cost = 0.7;
+          case '<= 3':
+            cost = 0.85;
+          case '<= 3.5':
+            cost = 1.0;
+        }
+          
     } else if (type == "metered") {
-      switch (weight){
-        case '< 1': 
-          cost = 0.5;
-        case '< 2':
-          cost = 0.65;
-        case '< 3':
-          cost = 0.8;
-        case '< 3.5':
-          cost = 0.95;
-      }  
+        switch (weight){
+          case '<= 1': 
+            cost = 0.5;
+          case '<= 2':
+            cost = 0.65;
+          case '<= 3':
+            cost = 0.8;
+          case '<= 3.5':
+            cost = 0.95;
+        } 
+
     } else if (type == "flats") {
-      switch (weight){
-        case '< 1': 
-          cost = 0.5;
-        case '< 2':
-          cost = 0.65;
-        case '< 3':
-          cost = 0.8;
-        case '< 3.5':
-          cost = 0.95;
-      }  
+        switch (weight){
+          case '<= 1': 
+            cost = 1.0;
+          case '<= 2':
+            cost = 1.2;
+          case '<= 3':
+            cost = 1.4;
+          case '<= 4':
+            cost = 1.6;
+          case '<= 5':
+            cost = 1.8;
+          case '<= 6':
+            cost = 2.0;
+          case '<= 7':
+            cost = 2.2;
+          case '<= 8':
+            cost = 2.4;
+          case '<= 9':
+            cost = 2.6;
+          case '<= 10':
+            cost = 2.8;
+          case '<= 11':
+            cost = 3.0;
+          case '<= 12':
+            cost = 3.2;
+          case '<= 13':
+            cost = 3.4;
+        }
+
     } else if (type == "first-class") {
-      switch (weight){
-        case '< 1': 
-          cost = 0.5;
-        case '< 2':
-          cost = 0.65;
-        case '< 3':
-          cost = 0.8;
-        case '< 3.5':
-          cost = 0.95;
-      }  
+        switch (weight){
+          case '<= 4':
+            cost = 3.8;
+          case '<= 8':
+            cost = 4.6;
+          case '<= 12':
+            cost = 5.3;
+          case '<= 13':
+            cost = 5.9;
+        }
+
     }
     
-    var cost = $('#cost');
-    cost.empty();
-    cost.append();  
+    cost = $('#cost');
+      
 }
 
 /* 
-
+cost.empty();
+    cost.append();
+    
 flats
 1	$1.00
 2	1.20
